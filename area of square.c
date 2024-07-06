bb3ca8050d19cef8d3b8b25c2531498689ca6286
Binaya Ginesh <binayaginesh@gmail.com>
2024-07-06 21:46:50 +0530

    .

---
+++ area of square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//area of square 
+int main()
+{
+    int side;
+    printf("enter the side of sqaure%d",side);
+    scanf("%d",&side);
+    printf("area of the square is%d",side*side);
+
+    return 0;
+}
