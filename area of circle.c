21289935fc945a9cbf57230d5a0bb081087c3ef8
Binaya Ginesh <binayaginesh@gmail.com>
2024-07-06 21:47:30 +0530

    .

---
+++ area of circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//area of circle 
+int main()
+{
+    int radius;
+    printf("please enter the radius of the circle");
+    scanf("%d",&radius);
+    printf("area of the circle is%f",3.14*radius*radius);
+
+    return 0;
+}
