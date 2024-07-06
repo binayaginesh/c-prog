fab43e01e337fff74e18453889d802e193c40080
Binaya Ginesh <binayaginesh@gmail.com>
2024-07-06 21:46:06 +0530

    .

---
+++ array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5],i;
+    printf("Enter array elements");
+    for(i=0;i<5;i++){
+        scanf("%d\n",&arr[i]);
+    }
+    printf("Entered values\n");
+    for(i=0;i<5;i++){
+        printf("%d\t",arr[i]);
+    }
+
+    return 0;
+}
