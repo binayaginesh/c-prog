caa1678a7381df9e93984c1a7d8f7fd5d1ccfbb0
Binaya Ginesh <binayaginesh@gmail.com>
2024-07-08 23:46:19 +0530

    bodmass

---
+++ bodmas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=5*2/2*3,b=5*2-2*3,
+    c=5*(2/2)*3,d=5+2/2*3;
+    printf("a=%d\n",a);
+    printf("b=%d\n",b);
+    printf("c=%d\n",c);
+    printf("d=%d\n",d);
+
+    return 0;
+}
