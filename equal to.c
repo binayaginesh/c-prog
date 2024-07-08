f0b908608f0dd12429d18c44090c733b2fbd937a
Binaya Ginesh <binayaginesh@gmail.com>
2024-07-08 23:47:44 +0530

    equal

---
+++ equal to.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   
+    printf("(1 indicates true)%d\n",4==4);
+    printf("(0 indicates false) %d\n",4==3);
+    printf("(it means not equal to)%d\n",4!=3);
+    
+   
+
+    return 0;
+}
